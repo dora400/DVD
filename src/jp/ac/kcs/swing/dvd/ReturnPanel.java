c22137f594a36e32911c47820eedbfbe0bab35e3
mid <mid@k302.kcsk.ac.jp>
2025-07-23 11:57:21 +0900

    testaddRentPanel.java

---
+++ src/jp/ac/kcs/swing/dvd/ReturnPanel.java
@@ -0,0 +1,42 @@
+package jp.ac.kcs.swing.dvd;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ReturnPanel extends JPanel {
+    public ReturnPanel(MainFrame frame) {
+    	setLayout(new GridLayout(4, 2));
+        //ボタンとか作成
+    	JTextField codeField = new JTextField(); 
+    	JButton returnBtn = new JButton("返却");
+    	JButton backBtn = new JButton("TOPへ");
+
+
+
+        //DB返却処理
+    	returnBtn.addActionListener(e -> {
+    		DB.returnBook(codeField.getText());
+    		JOptionPane.showMessageDialog(this, "返却が完了しました。");
+    	});
+
+
+
+        //TOPに戻るボタン
+    	backBtn.addActionListener(e -> frame.showPanel("TOP"));
+    	
+
+
+        //パネルに部品の追加
+    	add(new JLabel("書籍コード"));
+    	add(codeField);
+    	add(returnBtn);
+    	add(backBtn);
+
+
+    }
+}
